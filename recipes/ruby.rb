d1451665b20c3b86063c6e444911c5eaabc190a8
pikesley <github@orgraphone.org>
2015-09-09 14:53:05 +0100

    Weird, weird bug

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -7,3 +7,11 @@ if node['ruby-ng']['ruby_version'].to_s < '2'
 else
   include_recipe 'ruby-ng::dev'
 end
+
+# There is the weirdest fucking bug somewhere in ruby-ng
+# This directory gets chown'ed to root, but *only when we install ruby 2*
+directory '/home/vagrant/.gem/' do
+  owner 'vagrant'
+  group 'vagrant'
+  only_if { File.exists? '/home/vagrant/.gem/' }
+end
