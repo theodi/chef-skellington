2a378ab3459eb142987411ef981a7ffa238271b0
pikesley <github@orgraphone.org>
2015-09-30 16:52:23 +0100

    These can live here now

--- recipes/dependencies.rb
+++ recipes/dependencies.rb
@@ -1,3 +1,7 @@
+node.default['envbuilder']['base_dir'] = "/home/#{node['user']}"
+node.default['envbuilder']['owner'] = node['user']
+node.default['envbuilder']['group'] = node['user']
+
 include_recipe 'apt'
 include_recipe 'git'
 include_recipe 'nginx'
