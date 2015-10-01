91f162d006304a7d42116561b26549dbd7cb6695
pikesley <github@orgraphone.org>
2015-10-01 16:43:39 +0100

    Formatting

--- definitions/pre_bundle.rb
+++ definitions/pre_bundle.rb
@@ -1,20 +1,20 @@
 define :pre_bundle, :params => {} do
-    bash 'Symlink env' do
-      cwd params[:release_path]
-      user params[:name]
-      code <<-EOF
-        ln -sf /home/#{params[:name]}/env .env
-      EOF
-    end
+  bash 'Symlink env' do
+    cwd params[:release_path]
+    user params[:name]
+    code <<-EOF
+      ln -sf /home/#{params[:name]}/env .env
+    EOF
+  end
 
-    directory "/home/#{params[:name]}/#{params[:fully_qualified_domain_name]}/shared/config/" do
-      action :create
-      recursive true
-    end
+  directory "/home/#{params[:name]}/#{params[:fully_qualified_domain_name]}/shared/config/" do
+    action :create
+    recursive true
+  end
 
-    directory "/home/#{params[:name]}/#{params[:fully_qualified_domain_name]}/shared/log/" do
-      action :create
-      recursive true
-      user params[:name]
-    end
+  directory "/home/#{params[:name]}/#{params[:fully_qualified_domain_name]}/shared/log/" do
+    action :create
+    recursive true
+    user params[:name]
+  end
 end
