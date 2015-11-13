a90908bf88bec63e8b5123ff9d5c2545f42fed77
pikesley <github@orgraphone.org>
2015-11-13 16:55:05 +0000

    Oops

--- definitions/database_yml.rb
+++ definitions/database_yml.rb
@@ -1,10 +1,15 @@
-define :database_yml  , :params => {} do
+define :database_yml, :params => {} do
+  directory "/home/#{params[:name]}/#{params[:fully_qualified_domain_name]}/shared/config/" do
+    action :create
+    recursive true
+  end
+
   template "/home/#{params[:name]}/#{params[:fully_qualified_domain_name]}/shared/config/database.yml" do
     action :create
     variables(
       :mysql_host     => node['mysql']['host'],
       :mysql_database => node['mysql']['database'],
-      :mysql_username => node['mysql']['user'],
+      :mysql_username => node['mysql']['username'],
       :mysql_password => node['mysql']['password'],
       :mysql_pool     => node['mysql']['pool']
     )
