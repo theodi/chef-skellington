6dd3215caed18d2c07a42f4a19562943987a165f
pikesley <github@orgraphone.org>
2015-11-13 16:56:07 +0000

    Otherwise asset precomp fails

--- definitions/precompile_assets.rb
+++ definitions/precompile_assets.rb
@@ -2,6 +2,9 @@ define :precompile_assets, :params => {} do
   bash 'Precompiling assets' do
     cwd params[:cwd]
     user params[:user]
+    environment(
+      'RACK_ENV' => node['deployment']['rack_env']
+    )
     code <<-EOF
       bundle exec rake assets:precompile
     EOF
