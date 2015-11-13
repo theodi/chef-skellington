b45a62965b6f0b50d9297a9a0111dc34ae709215
pikesley <github@orgraphone.org>
2015-11-13 17:08:55 +0000

    This works now

--- recipes/attributes.rb
+++ recipes/attributes.rb
@@ -1,3 +1,7 @@
 node.default['envbuilder']['base_dir'] = "/home/#{node['user']}"
 node.default['envbuilder']['owner'] = node['user']
 node.default['envbuilder']['group'] = node['user']
+
+node.default['mysql']['database'] = node['user'] unless node['mysql']['database']
+node.default['mysql']['username'] = node['user'] unless node['mysql']['username']
+node.default['mysql']['password'] = node['user'] unless node['mysql']['password']
